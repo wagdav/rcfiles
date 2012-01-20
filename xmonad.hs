6ac6c243388393652c2184c51553e907fc7aa1cc
David Wagner <david.wagner@epfl.ch>
2012-01-20 14:51:02 +0100

    Simplify the configuration.

--- xmonad.hs
+++ xmonad.hs
@@ -1,39 +1,64 @@
 import XMonad
-
 import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.SetWMName
-import XMonad.Hooks.UrgencyHook
 import XMonad.Hooks.ManageDocks
+import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.Run(spawnPipe)
+import System.IO
 
-import XMonad.Prompt
-import XMonad.Prompt.Shell
-import XMonad.Prompt.XMonad
+import XMonad.Hooks.SetWMName
 import XMonad.Actions.CycleWS
 import XMonad.Actions.GridSelect
 
-import XMonad.Util.Run
 import XMonad.Util.Scratchpad
 import XMonad.Util.NamedScratchpad
 import qualified XMonad.StackSet as W
 
 import XMonad.Layout.NoBorders
 
-import qualified Data.Map as M
 import Control.Monad
-import System.IO
 
 -- The main function.
 main = do
     xmproc <- spawnPipe "xmobar"
-    xmonad $ withUrgencyHook NoUrgencyHook $ defaultConfig
-     { modMask = mod4Mask
-    , borderWidth=2
-    , manageHook = newManageHook
-    , startupHook = setWMName "LG3D"
-    , keys = newKeys
-    , logHook = myLogHook xmproc
-    , layoutHook = myLayout
-    }
+    xmonad $ defaultConfig
+        { modMask = mod4Mask -- Rebind Mod to the Windows key
+        , borderWidth=2
+        , manageHook = myManageHook
+        , startupHook = setWMName "LG3D"
+        , logHook = myLogHook xmproc
+        , layoutHook = myLayout
+        , workspaces = myWorkspaces
+        } `additionalKeys` myKeys
+
+
+myWorkspaces = ["1:web","2:local","3:local","4:lac","5:lac","6","7","8","9"]
+
+myKeys =
+        -- MPD keys
+        [ ((0, 0x1008ff14), spawn "mpc toggle") -- play/pause song
+        , ((0, 0x1008ff15), spawn "mpc stop")   -- stop song
+        , ((0, 0x1008ff16), spawn "mpc prev")   -- previous song
+        , ((0, 0x1008ff17), spawn "mpc next")   -- next song
+        -- CycleWS
+        , ((mod4Mask, xK_quoteleft), toggleSkip ["NSP"])
+        -- ScratchPad
+        , ((mod4Mask, xK_s), scratchpadSpawnAction defaultConfig)
+        , ((mod4Mask, xK_v), namedScratchpadAction scratchpads "editor")
+        , ((mod4Mask, xK_b), namedScratchpadAction scratchpads "editor-lac")
+        , ((mod4Mask, xK_Insert), namedScratchpadAction scratchpads "music")
+        , ((mod4Mask, xK_o), namedScratchpadAction scratchpads "mail")
+        -- GridSelect
+        , ((mod4Mask , xK_f), goToSelected defaultGSConfig)
+        ]
+        ++ switchNonGreedyView
+
+
+switchNonGreedyView = [
+    ((m .|. mod4Mask, k), windows $ f i) -- Replace 'mod4Mask' with your mod key of choice.
+    | (i, k) <- zip myWorkspaces [xK_1 .. xK_9]
+    , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
+    ]
+
 
 myLayout = avoidStruts $ tiled ||| Mirror tiled ||| Full
     where
@@ -42,57 +67,41 @@ myLayout = avoidStruts $ tiled ||| Mirror tiled ||| Full
         ratio = 1/2
         delta = 3/100
 
-myManageHook = composeAll
-    [ className =? "com-mathworks-util-PostVMInit" --> doFloat
-    , className =? "Ipython" --> doFloat
-    , title =? "File Transfers" --> doFloat
-    ]
-newManageHook =
+
+myManageHook =
     scratchpadManageHook (W.RationalRect 0.1 0.25 0.8 0.5) <+>
     namedScratchpadManageHook scratchpads <+>
-    myManageHook <+>
+    composeAll
+        [ className =? "com-mathworks-util-PostVMInit" --> doFloat
+        , className =? "Ipython" --> doFloat
+        , title =? "File Transfers" --> doFloat
+        , title =? "ImageJ" --> doFloat
+        , className =? "Blender" --> doFloat
+        ] <+>
+    manageDocks <+>
     manageHook defaultConfig
+
 
 -- It determines what's being written to the bar.
 myLogHook :: Handle -> X ()
 myLogHook h = dynamicLogWithPP $ customPP { ppOutput = hPutStrLn h }
 
 customPP :: PP
-customPP = defaultPP
-    { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">"
-    , ppTitle = xmobarColor "green" "" . shorten 80
-    , ppUrgent = xmobarColor "red" "" . wrap "!" "!"
-    , ppSort = fmap (.namedScratchpadFilterOutWorkspace) $ ppSort defaultPP
+customPP = xmobarPP
+    {   ppCurrent = xmobarColor "yellow" "" . wrap "[" "]",
+        ppTitle = xmobarColor "green" "" . shorten 80
+    ,   ppSort = fmap (.namedScratchpadFilterOutWorkspace) $ ppSort defaultPP
     }
 
--- Key bindings
-myKeys conf@(XConfig {XMonad.modMask = modm}) = 
-    -- MPD keys
-    [ ((0, 0x1008ff14), spawn "mpc toggle") -- play/pause song
-    , ((0, 0x1008ff15), spawn "mpc stop") -- stop song
-    , ((0, 0x1008ff16), spawn "mpc prev") -- previous song
-    , ((0, 0x1008ff17), spawn "mpc next") -- next song
-    -- CycleWS
-    , ((modm, xK_quoteleft), toggleSkip ["NSP"])
-    -- ScratchPad
-    , ((modm, xK_s), scratchpadSpawnAction defaultConfig)
-    , ((modm, xK_v), namedScratchpadAction scratchpads "editor")
-    , ((modm, xK_b), namedScratchpadAction scratchpads "editor-lac")
-    , ((modm, xK_Insert), namedScratchpadAction scratchpads "music")
-    -- GridSelect
-    , ((modm , xK_f), goToSelected defaultGSConfig)
-    ]
 
--- toggle any workspace but scratchpad
+-- Toggle any workspace but scratchpad
 toggleSkip :: [WorkspaceId] -> X ()
 toggleSkip skips = do
     hs <- gets (flip skipTags skips . W.hidden . windowset)
     unless (null hs) (windows . W.view . W.tag $ head hs)
 
-newKeys x = M.union
-     (keys defaultConfig x)
-     (M.fromList (myKeys x))
 
+-- Scratchpads
 scratchpads :: [NamedScratchpad]
 scratchpads =
     [ NS "editor" "gvim --role Editor --servername crpppc312"
@@ -103,6 +112,9 @@ scratchpads =
              (nonFloating)
     , NS "music" "x-terminal-emulator -title Music -e ncmpc --colors"
              (title =? "Music")
+             (customFloating $ W.RationalRect (1/6) (1/6) (2/3) (2/3))
+    , NS "mail" "x-terminal-emulator -title Mail -e mutt"
+             (title =? "Mail")
              (customFloating $ W.RationalRect (1/6) (1/6) (2/3) (2/3))
     ]
     where role = stringProperty "WM_WINDOW_ROLE"
